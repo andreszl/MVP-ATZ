14c2327f9bec30bcf6f76b574d58a8d1c6d5bba2
andreszl <andreszl.devcode@gmail.com>
2020-02-25 22:26:16 -0500

    video 1

--- app/src/main/java/com/example/tide/EditorActivity.java
+++ app/src/main/java/com/example/tide/EditorActivity.java
@@ -9,11 +9,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EditorActivity extends AppCompatActivity {
 
     EditText et_title, et_note;
     ProgressDialog progressDialog;
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +44,70 @@ public class EditorActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.save:
-                saveNota();
+                String title = et_title.getText().toString().trim();
+                String note = et_note.getText().toString().trim();
+                int color = -2184710;
+                if(title.isEmpty())
+                {
+                    et_title.setError("Please enter a title ");
+                }
+                else if (note.isEmpty())
+                {
+                    et_note.setError("Please enter a note ");
+                }
+                else {
+                    saveNote(title, note, color);
+                }
+                return true;
+                default:
+                     return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+
     }
 
-    private void saveNota() {
+    private void saveNote(final String title, final String note, final int color) {
        progressDialog.show();
+
+
+       apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+       Call<Note> call = apiInterface.saveNote(title, note, color);
+
+           call.enqueue(new Callback<Note>() {
+
+
+               @Override
+               public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+                  progressDialog.dismiss();
+
+                  if(response.isSuccessful() && response.body() != null )
+                  {
+                      Boolean success = response.body().getSuccess();
+
+                      if(success)
+                       {
+                          Toast.makeText(EditorActivity.this,
+                                  response.body().getMessage(),
+                                  Toast.LENGTH_SHORT).show();
+                          finish();
+
+                       }
+                      else
+                      {
+                          Toast.makeText(EditorActivity.this,
+                                  response.body().getMessage(),
+                                  Toast.LENGTH_SHORT).show();
+                      }
+                  }
+               }
+
+               @Override
+               public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+                   progressDialog.dismiss();
+                   Toast.makeText(EditorActivity.this,
+                           t.getLocalizedMessage(),
+                           Toast.LENGTH_SHORT).show();
+               }
+           });
     }
 }
+
